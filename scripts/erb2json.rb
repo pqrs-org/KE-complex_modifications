ecd893711c855c682c1f6a7696f7b67d372fa79e
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-10 01:17:10 +0900

    update emacs key bindings

--- scripts/erb2json.rb
+++ scripts/erb2json.rb
@@ -39,28 +39,81 @@ def to(events)
 end
 
 def frontmost_application(type, app_alias)
+  emacs_bundle_identifiers = [
+    '^org\.gnu\.Emacs$',
+    '^org\.gnu\.AquamacsEmacs$',
+    '^org\.gnu\.Aquamacs$',
+    '^org\.pqrs\.unknownapp.conkeror$',
+  ]
+
+  remote_desktop_bundle_identifiers = [
+    '^com\.microsoft\.rdc$',
+    '^com\.microsoft\.rdc\.mac$',
+    '^com\.microsoft\.rdc\.osx\.beta$',
+    '^net\.sf\.cord$',
+    '^com\.thinomenon\.RemoteDesktopConnection$',
+    '^com\.itap-mobile\.qmote$',
+    '^com\.nulana\.remotixmac$',
+    '^com\.p5sys\.jump\.mac\.viewer$',
+    '^com\.p5sys\.jump\.mac\.viewer\.web$',
+    '^com\.vmware\.horizon$',
+    '^com\.2X\.Client\.Mac$',
+  ]
+
+  terminal_bundle_identifiers = [
+    '^com\.apple\.Terminal$',
+    '^com\.googlecode\.iterm2$',
+    '^co\.zeit\.hyperterm$',
+    '^co\.zeit\.hyper$',
+  ]
+
+  vi_bundle_identifiers = [
+    '^org\.vim\.', # prefix
+  ]
+
+  virtual_machine_bundle_identifiers = [
+    '^com\.vmware\.fusion$',
+    '^com\.vmware\.horizon$',
+    '^com\.vmware\.view$',
+    '^com\.parallels\.desktop$',
+    '^com\.parallels\.vm$',
+    '^com\.parallels\.desktop\.console$',
+    '^org\.virtualbox\.app\.VirtualBoxVM$',
+    '^com\.vmware\.proxyApp\.', # prefix
+    '^com\.parallels\.winapp\.', # prefix
+  ]
+
+  x11_bundle_identifiers = [
+    '^org\.x\.X11$',
+    '^com\.apple\.x11$',
+    '^org\.macosforge\.xquartz\.X11$',
+    '^org\.macports\.X11$',
+  ]
+
   case app_alias
   when 'terminal'
     data = {
       'type' => type,
-      'bundle_identifiers' => [
-        '^com\.apple\.Terminal$',
-        '^com\.googlecode\.iterm2$',
-        '^co\.zeit\.hyperterm$',
-        '^co\.zeit\.hyper$',
-      ],
+      'bundle_identifiers' => terminal_bundle_identifiers,
     }
     JSON.generate(data)
   when 'emacs'
     data = {
       'type' => type,
-      'bundle_identifiers' => [
-        '^org\.gnu\.Emacs$',
-        '^org\.gnu\.AquamacsEmacs$',
-        '^org\.gnu\.Aquamacs$',
-        '^org\.pqrs\.unknownapp.conkeror$',
-      ],
+      'bundle_identifiers' => emacs_bundle_identifiers,
     }
+    JSON.generate(data)
+  when 'emacs_key_bindings_exception'
+    data = {
+      'type' => type,
+      'bundle_identifiers' => emacs_bundle_identifiers +
+                              remote_desktop_bundle_identifiers +
+                              terminal_bundle_identifiers +
+                              vi_bundle_identifiers +
+                              virtual_machine_bundle_identifiers +
+                              x11_bundle_identifiers,
+    }
+    JSON.generate(data)
   else
     ''
   end
