60d86c9e366c34bbd8c341ffe7f4e9491ce0b6b0
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-10 00:19:47 +0900

    update personal_tekezo

--- scripts/erb2json.rb
+++ scripts/erb2json.rb
@@ -38,5 +38,41 @@ def to(events)
   JSON.generate(data)
 end
 
+def frontmost_application(type, app_alias)
+  case app_alias
+  when 'terminal'
+    data = {
+      'type' => type,
+      'bundle_identifiers' => [
+        '^com\.apple\.Terminal$',
+        '^com\.googlecode\.iterm2$',
+        '^co\.zeit\.hyperterm$',
+        '^co\.zeit\.hyper$',
+      ],
+    }
+    JSON.generate(data)
+  when 'emacs'
+    data = {
+      'type' => type,
+      'bundle_identifiers' => [
+        '^org\.gnu\.Emacs$',
+        '^org\.gnu\.AquamacsEmacs$',
+        '^org\.gnu\.Aquamacs$',
+        '^org\.pqrs\.unknownapp.conkeror$',
+      ],
+    }
+  else
+    ''
+  end
+end
+
+def frontmost_application_if(app_alias)
+  frontmost_application('frontmost_application_if', app_alias)
+end
+
+def frontmost_application_unless(app_alias)
+  frontmost_application('frontmost_application_unless', app_alias)
+end
+
 template = ERB.new $stdin.read
 puts JSON.pretty_generate(JSON.parse(template.result))
