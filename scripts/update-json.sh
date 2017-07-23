b56507227d9a940c8129ed246ff9f2b5fbc5b838
Takayama Fumihiko <tekezo@pqrs.org>
2017-07-23 09:19:40 +0900

    remove dstfile if error

--- scripts/update-json.sh
+++ scripts/update-json.sh
@@ -3,7 +3,11 @@
 for srcfile in src/json/*.erb; do
   dstfile="docs/json/`basename $srcfile .erb`"
   if [ "$srcfile" -nt "$dstfile" ]; then
-    echo "$dstfile"
-    /usr/bin/ruby scripts/erb2json.rb < "$srcfile" > "$dstfile" || exit 1
+    if /usr/bin/ruby scripts/erb2json.rb < "$srcfile" > "$dstfile"; then
+      echo "$dstfile"
+    else
+      rm -f "$dstfile"
+      exit 1
+    fi
   fi
 done
