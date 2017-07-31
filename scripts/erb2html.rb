c2bd7a09c0b2902e69dd9f30b4ca28eb6b231c24
Kam Kuo <developer.kam@gmail.com>
2017-07-31 15:20:29 -0700

    Refactored index of mappings to group files

--- scripts/erb2html.rb
+++ scripts/erb2html.rb
@@ -8,7 +8,7 @@ include ERB::Util
 def import_button(json_file_path)
   json_path = json_file_path.gsub(/^docs\//, '')
 
-  "<a class=\"btn btn-primary btn-sm\" style=\"margin-left: 40px\" data-json-path=\"#{json_path}\">Import</a>"
+  "<a class=\"btn btn-primary btn-sm pull-right\" data-json-path=\"#{json_path}\">Import</a>"
 end
 
 def file_import_panel(json_file_path)
@@ -19,27 +19,25 @@ def file_import_panel(json_file_path)
     data = JSON.parse(f.read)
     title = h(data['title'])
     data['rules'].each do |rule|
-      rule_descriptions += '<li>' + h(rule['description']) + '</li>'
+      rule_descriptions += '<div class="list-group-item">' + h(rule['description']) + '</div>'
     end
   end
 
   extra_description_file_path = 'src/extra_descriptions/' + json_file_path.gsub(/^docs\/json\//, '') + '.html'
   if FileTest.exist?(extra_description_file_path)
     File.open(extra_description_file_path) do |f|
-      rule_descriptions += f.read
+      rule_descriptions += '<div class="list-group-item">' + f.read + '</div>'
     end
   end
 
   <<-EOS
     <div class="panel panel-default">
       <div class="panel-heading">
-        <h3 class="panel-title">#{title}</h3>
-      </div>
-      <div class="panel-body">
-        <ul>
-          #{rule_descriptions}
-        </ul>
+        <a class="panel-title btn btn-link" role="button" data-toggle="collapse" href="##{json_file_path.gsub(/^docs\/json\//, '').gsub(/.json/, '')}" aria-expanded="false" aria-controls="#{json_file_path.gsub(/^docs\/json\//, '').gsub(/.json/, '')}">#{title}</a>
         #{import_button(json_file_path)}
+      </div>
+      <div class="list-group collapse" id="#{json_file_path.gsub(/^docs\/json\//, '').gsub(/.json/, '')}">
+          #{rule_descriptions}
       </div>
     </div>
   EOS
