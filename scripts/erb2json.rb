9a4821520b47d11b4ce0536856123b537cfcccee
Jesper Nellemann Jakobsen <jesper.nellemann@gmail.com>
2017-08-01 23:24:55 +0200

    Use keyword arguments for each_key function

--- scripts/erb2json.rb
+++ scripts/erb2json.rb
@@ -45,7 +45,7 @@ def to(events)
 end
 
 
-def _each_key(source_keys_list, dest_keys_list, from_mandatory_modifiers, from_optional_modifiers, to_pre_events, to_modifiers, to_post_events, conditions)
+def each_key(source_keys_list: :source_keys_list, dest_keys_list: :dest_keys_list, from_mandatory_modifiers: [], from_optional_modifiers: [], to_pre_events: [], to_modifiers: [], to_post_events: [], conditions: [], as_json: false)
   data = []
   source_keys_list.each_with_index do |from_key,index|
     to_key = dest_keys_list[index]
