1faf629223459556854b4f024230d9263b5b7d34
Takayama Fumihiko <tekezo@pqrs.org>
2017-12-09 03:35:03 +0900

    use vue.js in index.html

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,10 +1,61 @@
-$(function() {
-  console.log('hello');
-  $('a[data-json-path]').each(function() {
-    var $this = $(this);
-    var json_path = $this.data('json-path');
-    var json_url = encodeURIComponent(location.href.replace(/[^/]+$/, '') + json_path);
-    var url = 'karabiner://karabiner/assets/complex_modifications/import?url=' + json_url;
-    $this.attr('href', url);
+{
+  let groups = [];
+
+  let app4 = new Vue({
+    el: '#app-4',
+    data: {
+      groups: groups
+    }
   });
-});
+
+  let baseName = function(path) {
+    let base = path.substring(path.lastIndexOf('/') + 1);
+    if (base.lastIndexOf(".") != -1) {
+      base = base.substring(0, base.lastIndexOf("."));
+    }
+    return base;
+  };
+
+  let jsonUrl = function(path) {
+    let url = encodeURIComponent(location.href.replace(/[^/]+$/, '') + path);
+    return 'karabiner://karabiner/assets/complex_modifications/import?url=' + url;
+  };
+
+  let getFile = function(path, groupIndex, fileIndex) {
+    axios.get(path).then(function(response) {
+      let f = {
+        title: response.data.title,
+        id: baseName(path),
+        importUrl: jsonUrl(path),
+        rules: []
+      };
+
+      response.data.rules.forEach(function(r) {
+        f.rules.push(r.description);
+      });
+
+      Vue.set(groups[groupIndex].files, fileIndex, f);
+    }).catch(function(error) {
+      console.log(error);
+    });
+  };
+
+  axios.get('groups.json')
+    .then(function(response) {
+      response.data.index.forEach(function(group, groupIndex) {
+        let g = {
+          name: group.name,
+          id: group.id,
+          files: []
+        };
+        groups.push(g);
+
+        group.files.forEach(function(file, fileIndex) {
+          getFile(file.path, groupIndex, fileIndex);
+        });
+      });
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
