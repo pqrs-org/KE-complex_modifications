13dd5e2fb6a555d1e8dd2edfabdecf34efe27743
Takayama Fumihiko <tekezo@pqrs.org>
2018-03-29 01:35:58 +0900

    test docs/groups.json

--- scripts/apply-lint.sh
+++ scripts/apply-lint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+ruby -rjson -e "open('docs/groups.json') {|f| JSON.parse(f.read) }" || exit 1
+
 for srcfile in docs/json/*.json; do
   echo "check $srcfile"
   scripts/lint.rb < "$srcfile" || exit 1
