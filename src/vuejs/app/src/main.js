51a599f3626dc121aef3b46333caf68202d6ae57
Takayama Fumihiko <tekezo@pqrs.org>
2018-04-23 00:59:14 +0900

    import vue-awesome

--- src/vuejs/app/src/main.js
+++ src/vuejs/app/src/main.js
@@ -5,9 +5,13 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import BootstrapVue from 'bootstrap-vue'
 
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
 Vue.config.productionTip = false
 
 Vue.use(BootstrapVue)
+Vue.component('icon', Icon)
 
 new Vue({
   render: h => h(App)
