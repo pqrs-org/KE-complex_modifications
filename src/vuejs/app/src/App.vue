5d1cc9850e7109b31a241d1e39ec4ce112e64a34
Takayama Fumihiko <tekezo@pqrs.org>
2018-04-27 01:53:38 +0900

    remove style

--- src/vuejs/app/src/App.vue
+++ src/vuejs/app/src/App.vue
@@ -16,9 +16,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
+
 </style>
