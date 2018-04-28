060d77d184c0376d62156d71034d1f91d0bac436
Takayama Fumihiko <tekezo@pqrs.org>
2018-04-28 14:20:09 +0900

    support Collapse/Expand button

--- src/vuejs/app/src/components/Index.vue
+++ src/vuejs/app/src/components/Index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-    <b-navbar toggleable="md" type="dark" variant="dark">
+    <b-navbar toggleable="md"
+              type="dark"
+              variant="dark">
       <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
       <b-navbar-brand href="./">Karabiner-Elements complex_modifications rules</b-navbar-brand>
-      <b-collapse is-nav id="nav_collapse">
+      <b-collapse is-nav
+                  id="nav_collapse">
         <b-navbar-nav class="ml-auto">
-          <b-nav-item href="https://github.com/pqrs-org/KE-complex_modifications" target="_blank">
+          <b-nav-item href="https://github.com/pqrs-org/KE-complex_modifications"
+                      target="_blank">
             <icon name="external-link-alt"></icon> GitHub
           </b-nav-item>
         </b-navbar-nav>
@@ -16,27 +20,56 @@
       <b-list-group class="toc">
         <b-list-group-item variant="info">Table of Contents</b-list-group-item>
 
-        <b-list-group-item v-for="group in groups" :key="group.id" v-cloak>
+        <b-list-group-item v-for="group in groups"
+                           :key="group.id"
+                           v-cloak>
           <a :href="'#' + group.id">{{ group.name }}</a>
-          <b-badge variant="secondary" class="float-right">{{ group.files.length }}</b-badge>
+          <b-badge variant="secondary"
+                   class="float-right">{{ group.files.length }}</b-badge>
         </b-list-group-item>
       </b-list-group>
       <div style="margin-top: 1rem; margin-bottom: 3rem">
-        <a href="#" @click="$('.collapse').collapse('toggle'); false;">Expand/Collapse All</a>
+        <div v-if="allRulesVisible">
+          <b-btn variant="secondary"
+                 @click="setAllRuleVisibilities(false)">Collapse All</b-btn>
+        </div>
+        <div v-else>
+          <b-btn variant="secondary"
+                 @click="setAllRuleVisibilities(true)">Expand All</b-btn>
+        </div>
       </div>
-      <div class="card-outer" v-for="group in groups" :key="group.id" :id="group.id" v-cloak>
-        <b-card class="border-info" :header="group.name" header-bg-variant="info" header-text-variant="white">
-          <div v-for="file in group.files" :key="file.id">
-            <div class="rule-card-outer" v-if="file" :id="file.id">
+      <div class="card-outer"
+           v-for="group in groups"
+           :key="group.id"
+           :id="group.id"
+           v-cloak>
+        <b-card class="border-info"
+                :header="group.name"
+                header-bg-variant="info"
+                header-text-variant="white">
+          <div v-for="file in group.files"
+               :key="file.id">
+            <div class="rule-card-outer"
+                 v-if="file"
+                 :id="file.id">
               <b-card no-body>
                 <b-card-header>
-                  <span class="rule-title" v-b-toggle="file.id + '-list-group'">{{ file.title }}</span>
-                  <a class="btn btn-primary btn-sm float-right" :href="file.importUrl">Import</a>
+                  <span class="rule-title"
+                        v-b-toggle="file.id + '-list-group'">{{ file.title }}</span>
+                  <a class="btn btn-primary btn-sm float-right"
+                     :href="file.importUrl">Import</a>
                 </b-card-header>
-                <b-collapse :id="file.id + '-list-group'">
+                <b-collapse :id="file.id + '-list-group'"
+                            :visible="ruleVisibilities[file.id]"
+                            @shown="setRuleVisibility(file.id, true)"
+                            @hidden="setRuleVisibility(file.id, false)">
                   <div class="list-group list-group-flush">
-                    <div class="list-group-item" v-for="rule in file.rules" :key="rule.id">{{ rule }}</div>
-                    <div class="list-group-item" v-if="file.extraDescription" v-html="file.extraDescription"></div>
+                    <div class="list-group-item"
+                         v-for="rule in file.rules"
+                         :key="rule.id">{{ rule.description }}</div>
+                    <div class="list-group-item"
+                         v-if="file.extraDescription"
+                         v-html="file.extraDescription"></div>
                   </div>
                 </b-collapse>
               </b-card>
@@ -58,6 +91,8 @@ export default {
   data() {
     return {
       groups: [],
+      allRulesVisible: false,
+      ruleVisibilities: {},
       fetchTotalNumber: 0,
       fetchedCount: 0,
       pageName: this.baseName(window.location.pathname)
@@ -141,6 +176,8 @@ export default {
             f.rules = []
           }
 
+          self.$set(self.ruleVisibilities, f.id, false)
+
           response.data.rules.forEach(function(r) {
             f.rules.push({
               id: 'rule-' + groupIndex + '-' + fileIndex + '-' + f.rules.length,
@@ -169,6 +206,32 @@ export default {
         .catch(function(error) {
           console.log(error)
         })
+    },
+
+    updateAllRulesVisible() {
+      this.allRulesVisible = true
+      for (const v of Object.values(this.ruleVisibilities)) {
+        if (!v) {
+          this.allRulesVisible = false
+          return
+        }
+      }
+    },
+
+    setAllRuleVisibilities(value) {
+      let ruleVisibilities = {}
+      for (const id of Object.keys(this.ruleVisibilities)) {
+        ruleVisibilities[id] = value
+      }
+      this.ruleVisibilities = ruleVisibilities
+
+      this.updateAllRulesVisible()
+    },
+
+    setRuleVisibility(id, value) {
+      this.$set(this.ruleVisibilities, id, value)
+
+      this.updateAllRulesVisible()
     }
   }
 }
