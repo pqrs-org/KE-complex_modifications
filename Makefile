fd0bde943ad2f89acd590173a6f1f9468d21cf80
Takayama Fumihiko <tekezo@pqrs.org>
2019-03-09 12:31:21 +0900

    add lint-groups.rb

--- Makefile
+++ Makefile
@@ -1,11 +1,13 @@
 all:
 	bash scripts/update-json.sh
+	ruby scripts/lint-groups.rb
 	(cd docs && ruby ../scripts/make-distjs.rb > dist.json)
 
 rebuild:
 	touch src/json/*
 	$(MAKE) all
 	scripts/apply-lint.sh docs/json/*.json
+	ruby scripts/lint-groups.rb
 
 server:
 	ruby scripts/dev-server.rb
---
+++ scripts/lint-groups.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+require 'json'
+
+files_in_groups = []
+open(File.join(__dir__, '..', 'docs', 'groups.json')) do |f|
+  data = JSON.parse(f.read)
+  data.each_value do |group|
+    group.each do |category|
+      category['files'].each do |file|
+        files_in_groups << File.basename(file['path'])
+      end
+    end
+  end
+end
+files_in_groups.sort!
+
+json_files = []
+# Exclude git ignored files
+`git ls-files #{__dir__}/../docs/json`.split(/\n/).each do |file_path|
+  json_files << File.basename(file_path)
+end
+json_files.sort!
+
+missing_entries = json_files - files_in_groups
+unless missing_entries.empty?
+  puts ''
+  puts '----------------------------------------'
+  puts 'ERROR:'
+  puts 'There are some files are not included in docs/groups.json.'
+  puts 'Please add them into docs/groups.json.'
+  puts '----------------------------------------'
+  puts ''
+
+  missing_entries.each do |e|
+    puts "- #{e}"
+  end
+
+  puts ''
+
+  exit 1
+end
+
+exit 0
