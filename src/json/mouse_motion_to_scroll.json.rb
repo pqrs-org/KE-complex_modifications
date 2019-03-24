d9f756755bf8545b3c0f10fd523f227997a75bb9
Takayama Fumihiko <tekezo@pqrs.org>
2019-03-25 01:08:02 +0900

    update mouse_motion_to_scroll.json

--- src/json/mouse_motion_to_scroll.json.rb
+++ src/json/mouse_motion_to_scroll.json.rb
@@ -16,7 +16,7 @@ def main
     'maintainer' => 'tekezo',
     'rules' => [
       {
-        'description' => 'Change button4 + mouse motion to scroll wheel (Available since Karabiner-Elements v12.2.6)',
+        'description' => 'Change button4 + mouse motion to scroll wheel (rev 1) (Available since Karabiner-Elements v12.2.6)',
         'manipulators' => [
           {
             'type' => 'basic',
@@ -51,7 +51,42 @@ def main
         ].flatten,
       },
       {
-        'description' => 'Change control + mouse motion to scroll wheel (Available since Karabiner-Elements v12.2.6)',
+        'description' => 'Change button5 + mouse motion to scroll wheel (rev 1) (Available since Karabiner-Elements v12.2.6)',
+        'manipulators' => [
+          {
+            'type' => 'basic',
+            'from' => {
+              'pointing_button' => 'button5',
+              'modifiers' => {
+                'optional' => [
+                  'any',
+                ],
+              },
+            },
+            'to' => [
+              Karabiner.set_variable('enable_mouse_motion_to_scroll', 1),
+            ],
+            'to_after_key_up' => [
+              Karabiner.set_variable('enable_mouse_motion_to_scroll', 0),
+            ],
+          },
+          {
+            'type' => 'mouse_motion_to_scroll',
+            'from' => {
+              'modifiers' => {
+                'optional' => [
+                  'any',
+                ],
+              },
+            },
+            'conditions' => [
+              Karabiner.variable_if('enable_mouse_motion_to_scroll', 1),
+            ],
+          },
+        ].flatten,
+      },
+      {
+        'description' => 'Change control + mouse motion to scroll wheel (rev 1) (Available since Karabiner-Elements v12.2.6)',
         'manipulators' => [
           {
             'type' => 'mouse_motion_to_scroll',
