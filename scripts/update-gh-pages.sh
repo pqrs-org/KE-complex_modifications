15ba2b645a6feeb0cd3428964aed955cf8d023d6
Takayama Fumihiko <tekezo@pqrs.org>
2020-02-26 23:38:55 +0900

    gh-pages-ke-complex-modifications -> gh-pages-ke-complex-modifications.pqrs.org

--- scripts/update-gh-pages.sh
+++ scripts/update-gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if git clone --depth 1 git@github.com:pqrs-org/gh-pages-ke-complex-modifications.git; then
-  destdir=gh-pages-ke-complex-modifications
+if git clone --depth 1 git@github.com:pqrs-org/gh-pages-ke-complex-modifications.pqrs.org.git; then
+  destdir=gh-pages-ke-complex-modifications.pqrs.org
   revision=$(git rev-parse HEAD)
   echo $revision >$destdir/source-revision
   rsync -Lav --delete --exclude CNAME public/ $destdir/docs
@@ -10,7 +10,8 @@ if git clone --depth 1 git@github.com:pqrs-org/gh-pages-ke-complex-modifications
     git config user.email "tekezo@pqrs.org" &&
     git add -A &&
     git commit -m "pqrs-org/KE-complex_modifications@$revision" &&
-    git push)
+    echo moge)
+  #  git push)
 else
-  echo "Skip updating pqrs-org/gh-pages-ke-complex-modifications since git clone is failed."
+  echo "Skip updating pqrs-org/gh-pages-ke-complex-modifications.pqrs.org since git clone is failed."
 fi
