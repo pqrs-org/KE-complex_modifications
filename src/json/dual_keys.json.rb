0357b2dd0d3b8113ad8d8d7967e1c412e1bbf2d1
Marlon Richert <marlonrichert@users.noreply.github.com>
2020-10-17 22:39:36 +0300

    Use the home row for Dual Keys

--- src/json/dual_keys.json.rb
+++ src/json/dual_keys.json.rb
@@ -12,22 +12,22 @@ require_relative '../lib/karabiner.rb'
 
 def main
   puts JSON.pretty_generate(
-    "title" => "Dual keys (to relieve stress from your pinkies)",
+    "title" => "Dual-purpose keys to relieve stress from your pinkies",
     "maintainers" => ["marlonrichert"],
     "rules" => [
       {
-        "description" => "Dual keys",
+        "description" => "Dual Keys",
         "manipulators" => [
           generate_dual_key_rule("left_command", "tab", "left_shift"),
           generate_dual_key_rule("right_command", "hyphen", "right_shift"),
-          generate_dual_key_rule("left_option", "return_or_enter", "left_command"),
-          generate_dual_key_rule("right_option", "equal_sign", "right_command"),
-          generate_dual_key_rule("grave_accent_and_tilde", "grave_accent_and_tilde", "left_control"),
-          generate_dual_key_rule("slash", "slash", "right_control"),
-          generate_dual_key_rule("caps_lock", "backslash", "left_option"),
-          generate_dual_key_rule("quote", "quote", "right_option"),
-          generate_single_key_rule("left_control", "open_bracket"),
-          generate_single_key_rule("left_arrow", "close_bracket"),
+          generate_dual_key_rule("left_option", "return_or_enter", "left_option"),
+          generate_dual_key_rule("right_option", "equal_sign", "right_option"),
+          generate_dual_key_rule("f", "f", "left_command"),
+          generate_dual_key_rule("j", "j", "right_command"),
+          generate_dual_key_rule("d", "d", "left_control"),
+          generate_dual_key_rule("k", "k", "right_control"),
+          generate_single_key_rule("tab", "escape"),
+          generate_single_key_rule("caps_lock", "backslash"),
         ],
       },
     ],
