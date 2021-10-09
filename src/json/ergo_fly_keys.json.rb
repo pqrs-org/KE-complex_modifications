22908694181dfa155d4b83c2afcf882e8ce597aa
Marlon Richert <marlonrichert@users.noreply.github.com>
2021-10-09 18:03:55 +0300

    Reduce left ring finger strain in Ergo Fly Keys

--- src/json/ergo_fly_keys.json.rb
+++ src/json/ergo_fly_keys.json.rb
@@ -26,125 +26,75 @@ end
 def rules(trigger_key)
   variable = "ergo_fly_keys_#{trigger_key}"
   [
-    single_key("tab",           [ { "key_code" => "x", "modifiers" => ["right_control"], } ] ),
-    single_key("open_bracket",  [ { "key_code" => "x", "modifiers" => ["right_control"], } ] ),
-
-    dual_key("right_command", "hyphen",     "right_shift"),
-    dual_key("right_option",  "equal_sign", "right_command"),
-
     dual_key("left_option",   "open_bracket",   "left_command"),
     dual_key("left_command",  "close_bracket",  "left_shift"),
+    dual_key("right_command", "hyphen",         "right_shift"),
+    dual_key("right_option",  "equal_sign",     "right_command"),
 
     dual_key("caps_lock", "grave_accent_and_tilde", "left_option"),
     dual_key("quote",     "quote",                  "right_option"),
 
-    dual_key("slash",                   "slash",     "right_control"),
     dual_key("grave_accent_and_tilde",  "backslash", "left_control"),
     dual_key("left_shift",              "backslash", "left_control"),
+    dual_key("slash",                   "slash",     "right_control"),
 
     two_part_trigger_combo(trigger_key, variable, "b", "tab", []),
 
-    two_part_trigger_combo(trigger_key, variable, "i", "up_arrow", []),
-    two_part_trigger_combo(trigger_key, variable, "k", "down_arrow", []),
-    two_part_trigger_combo(trigger_key, variable, "j", "left_arrow", []),
-    two_part_trigger_combo(trigger_key, variable, "l", "right_arrow", []),
-    four_part_trigger_combo(
-        trigger_key, variable, "u",
+    two_part_trigger_combo( trigger_key, variable, "i", "up_arrow", []),
+    two_part_trigger_combo( trigger_key, variable, "k", "down_arrow", []),
+    two_part_trigger_combo( trigger_key, variable, "j", "left_arrow", []),
+    two_part_trigger_combo( trigger_key, variable, "l", "right_arrow", []),
+    four_part_trigger_combo(trigger_key, variable, "u",
         [ { "key_code" => "left_arrow", "modifiers" => ["left_option"], } ],
-        [ { "key_code" => "escape" }, { "key_code" => "b" }, ]
-    ),
-    four_part_trigger_combo(
-        trigger_key, variable, "o",
+        [ { "key_code" => "escape" }, { "key_code" => "b" }, ] ),
+    four_part_trigger_combo(trigger_key, variable, "o",
         [ { "key_code" => "right_arrow", "modifiers" => ["left_option"], } ],
-        [ { "key_code" => "escape" }, { "key_code" => "f"}, ]
-    ),
-    two_part_trigger_combo(
-        trigger_key, variable, "h",
-        "a", ["right_control"]
-    ),
-    two_part_trigger_combo(
-        trigger_key, variable, "semicolon",
-        "e", ["right_control"]
-    ),
-    four_part_trigger_combo(
-      trigger_key, variable, "y",
+        [ { "key_code" => "escape" }, { "key_code" => "f"}, ] ),
+    two_part_trigger_combo( trigger_key, variable, "h",         "a", ["right_control"] ),
+    two_part_trigger_combo( trigger_key, variable, "semicolon", "e", ["right_control"] ),
+    four_part_trigger_combo(trigger_key, variable, "y",
       [ { "key_code" => "left_arrow", "modifiers" => ["left_command"], } ],
-      [ { "key_code" => "escape" }, { "key_code" => "b", "modifiers" => ["right_control"], }, ]
-  ),
-    four_part_trigger_combo(
-      trigger_key, variable, "p",
+      [ { "key_code" => "escape" }, { "key_code" => "b", "modifiers" => ["right_control"], }, ] ),
+    four_part_trigger_combo(trigger_key, variable, "p",
       [ { "key_code" => "right_arrow", "modifiers" => ["left_command"], } ],
-      [ { "key_code" => "escape" }, { "key_code" => "f", "modifiers" => ["right_control"], }, ]
-  ),
+      [ { "key_code" => "escape" }, { "key_code" => "f", "modifiers" => ["right_control"], }, ] ),
+    two_part_trigger_combo( trigger_key, variable, "n",       "home",       []),
+    two_part_trigger_combo( trigger_key, variable, "m",       "page_up",    []),
+    two_part_trigger_combo( trigger_key, variable, "comma",   "page_down",  []),
+    two_part_trigger_combo( trigger_key, variable, "period",  "end",        []),
 
-    two_part_trigger_combo(trigger_key, variable, "e", "escape", []),
-    two_part_trigger_combo(trigger_key, variable, "d", "return_or_enter", []),
-    two_part_trigger_combo(trigger_key, variable, "s", "delete_or_backspace", []),
-    two_part_trigger_combo(trigger_key, variable, "f", "delete_forward", []),
-    four_part_trigger_combo(
-        trigger_key, variable, "w",
+    two_part_trigger_combo( trigger_key, variable, "d", "delete_or_backspace", []),
+    two_part_trigger_combo( trigger_key, variable, "f", "delete_forward", []),
+    four_part_trigger_combo(trigger_key, variable, "e",
         [ { "key_code" => "delete_or_backspace", "modifiers" => ["left_option"], } ],
-        [ { "key_code" => "escape" }, { "key_code" => "h", "modifiers" => ["left_control"], } ]
-    ),
-    four_part_trigger_combo(
-        trigger_key, variable, "r",
+        [ { "key_code" => "escape" }, { "key_code" => "delete_or_backspace" } ] ),
+    four_part_trigger_combo(trigger_key, variable, "r",
         [ { "key_code" => "delete_forward", "modifiers" => ["left_option"], } ],
-        [ { "key_code" => "escape" }, { "key_code" => "d" }, ]
-    ),
-    four_part_trigger_combo(
-        trigger_key, variable, "a",
-        [
-          { "key_code" => "a", "modifiers" => ["right_control", "right_shift"], },
-          { "key_code" => "k", "modifiers" => ["left_control"], },
-        ],
-        [ { "key_code" => "u", "modifiers" => ["left_control"], } ]
-    ),
-    two_part_trigger_combo(
-        trigger_key, variable, "g",
-        "k", ["left_control"]
-    ),
-
-    four_part_trigger_combo(
-        trigger_key, variable, "q",
-        [
-          { "key_code" => "delete_or_backspace", "modifiers" => ["left_command"], },
-        ],
-        [ { "key_code" => "w", "modifiers" => ["left_control"], } ]
-    ),
-    four_part_trigger_combo(
-        trigger_key, variable, "t",
-        [
-          { "key_code" => "right_arrow", "modifiers" => ["left_command","left_shift"], },
-          { "key_code" => "delete_forward" },
-        ],
-        [ { "key_code" => "escape" }, { "key_code" => "d", "modifiers" => ["left_control"], } ]
-    ),
-
-    two_part_trigger_combo(trigger_key, variable, "z", "up_arrow",   ["left_command"]),
-    four_part_trigger_combo(
-        trigger_key, variable, "x",
+        [ { "key_code" => "escape" }, { "key_code" => "d" }, ] ),
+    four_part_trigger_combo(trigger_key, variable, "s",
+        [ { "key_code" => "a", "modifiers" => ["right_control", "right_shift"], },
+          { "key_code" => "k", "modifiers" => ["left_control"], }, ],
+        [ { "key_code" => "u", "modifiers" => ["left_control"], } ] ),
+    two_part_trigger_combo( trigger_key, variable, "g", "k", ["left_control"]),
+    four_part_trigger_combo(trigger_key, variable, "w",
+        [ { "key_code" => "delete_or_backspace", "modifiers" => ["left_command"], }, ],
+        [ { "key_code" => "w", "modifiers" => ["left_control"], } ] ),
+    four_part_trigger_combo(trigger_key, variable, "t",
+        [ { "key_code" => "right_arrow", "modifiers" => ["left_command","left_shift"], },
+          { "key_code" => "delete_forward" }, ],
+        [ { "key_code" => "escape" }, { "key_code" => "d", "modifiers" => ["left_control"], } ] ),
+    two_part_trigger_combo( trigger_key, variable, "z", "escape", []),
+    two_part_trigger_combo( trigger_key, variable, "x", "up_arrow",   ["left_command"]),
+    two_part_trigger_combo( trigger_key, variable, "c", "down_arrow", ["left_command"]),
+    two_part_trigger_combo( trigger_key, variable, "v", "return_or_enter", []),
+    four_part_trigger_combo(trigger_key, variable, "q",
         [ { "key_code" => "up_arrow", "modifiers" => ["left_option"], }, ],
-        [
-          { "key_code" => "escape" },
-          { "key_code" => "comma", "modifiers" => ["left_shift"], }
-        ]
-    ),
-    four_part_trigger_combo(
-        trigger_key, variable, "c",
-        [
-          { "key_code" => "down_arrow", "modifiers" => ["left_option"], },
-        ],
-        [
-          { "key_code" => "escape" },
-          { "key_code" => "period", "modifiers" => ["left_shift"], }
-        ]
-    ),
-    two_part_trigger_combo(trigger_key, variable, "v", "down_arrow", ["left_command"]),
-
-    two_part_trigger_combo(trigger_key, variable, "n",      "home",       []),
-    two_part_trigger_combo(trigger_key, variable, "m",      "page_up",    []),
-    two_part_trigger_combo(trigger_key, variable, "comma",  "page_down",  []),
-    two_part_trigger_combo(trigger_key, variable, "period", "end",        []),
+        [ { "key_code" => "escape" },
+          { "key_code" => "comma", "modifiers" => ["left_shift"], } ] ),
+    four_part_trigger_combo(trigger_key, variable, "a",
+        [ { "key_code" => "down_arrow", "modifiers" => ["left_option"], }, ],
+        [ { "key_code" => "escape" },
+          { "key_code" => "period", "modifiers" => ["left_shift"], } ] ),
 
     trigger_key(trigger_key, variable),
   ].flatten
