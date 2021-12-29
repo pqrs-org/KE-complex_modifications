6505c8fa8462cfe1b57f32aa29cdcfc03b9c0ea7
Takayama Fumihiko <tekezo@pqrs.org>
2021-12-29 09:21:17 +0900

    Update personal_tekezo (Add Post command+click when fn is pressed alone)

--- src/json/personal_tekezo.json.rb
+++ src/json/personal_tekezo.json.rb
@@ -19,7 +19,7 @@ def main
     'maintainers' => ['tekezo'],
     'rules' => [
       {
-        'description' => 'Personal rules (@tekezo) (rev 40)',
+        'description' => 'Personal rules (@tekezo) (rev 41)',
         'available_since' => '13.6.0',
         'manipulators' =>
         core_configuration +
@@ -27,7 +27,6 @@ def main
         mouse +
         extra_cursor +
         option_hyphen +
-        media_controls +
         app_virtual_machine +
         app_finder +
         app_terminal +
@@ -41,6 +40,31 @@ end
 
 def core_configuration
   [
+    ########################################
+    # fn
+    ########################################
+
+    # Post command+click when fn is pressed alone
+    {
+      'type' => 'basic',
+      'from' => {
+        'key_code' => 'fn',
+        'modifiers' => Karabiner.from_modifiers,
+      },
+      'to' => [
+        { 'key_code' => 'fn' },
+      ],
+      'to_if_alone' => [
+        {
+          'pointing_button' => 'button1',
+          'modifiers' => ['left_command'],
+        },
+      ],
+      'parameters' => {
+        'basic.to_if_alone_timeout_milliseconds' => 250,
+      },
+    },
+
     ########################################
     # left_control
     ########################################
@@ -145,7 +169,7 @@ def core_configuration
     },
 
     ########################################
-    # fn
+    # left_command, left_option
     ########################################
 
     # input source switch
@@ -468,57 +492,6 @@ def option_hyphen
           'key_code' => 'equal_sign',
           'repeat' => false,
         },
-      ],
-    },
-  ]
-end
-
-def media_controls
-  # Change fn+page_up,page_down to brightness control
-  # Change page_up,page_down to volume control
-  [
-    # fn+page_down
-    {
-      'type' => 'basic',
-      'from' => {
-        'key_code' => 'page_down',
-        'modifiers' => Karabiner.from_modifiers(['fn'], ['any']),
-      },
-      'to' => [
-        { 'consumer_key_code' => 'display_brightness_decrement' },
-      ],
-    },
-    # fn+page_up
-    {
-      'type' => 'basic',
-      'from' => {
-        'key_code' => 'page_up',
-        'modifiers' => Karabiner.from_modifiers(['fn'], ['any']),
-      },
-      'to' => [
-        { 'consumer_key_code' => 'display_brightness_increment' },
-      ],
-    },
-    # page_down
-    {
-      'type' => 'basic',
-      'from' => {
-        'key_code' => 'page_down',
-        'modifiers' => Karabiner.from_modifiers([], ['any']),
-      },
-      'to' => [
-        { 'consumer_key_code' => 'volume_decrement' },
-      ],
-    },
-    # page_up
-    {
-      'type' => 'basic',
-      'from' => {
-        'key_code' => 'page_up',
-        'modifiers' => Karabiner.from_modifiers([], ['any']),
-      },
-      'to' => [
-        { 'consumer_key_code' => 'volume_increment' },
       ],
     },
   ]
