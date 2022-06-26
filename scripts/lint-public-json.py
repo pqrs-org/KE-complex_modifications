d8a541e7c4e57ecfa12f233a08e99fe3c536f5f9
Takayama Fumihiko <tekezo@pqrs.org>
2022-06-26 16:18:01 +0900

    Add scripts/lint-public-json.py

---
+++ scripts/lint-public-json.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+
+import glob
+import json
+import os
+import re
+import subprocess
+import sys
+
+publicJsonDirectory = sys.argv[1] if len(sys.argv) > 1 else ""
+if not os.path.isdir(publicJsonDirectory):
+    print('public/json is not found')
+    exit(1)
+
+#
+# Check files
+#
+
+filePaths = glob.glob("{}/*".format(publicJsonDirectory))
+for filePath in filePaths:
+    #
+    # Check file extension
+    #
+
+    if not re.search(r'\.json$', filePath):
+        basename = os.path.basename(filePath)
+        print('')
+        print('----------------------------------------')
+        print('ERROR:')
+        print("Please rename {}/{} to {}/{}.json".format(
+            publicJsonDirectory,
+            basename,
+            publicJsonDirectory,
+            basename))
+        print('----------------------------------------')
+        print('')
+        exit(1)
+
+    #
+    # Check json validity
+    #
+
+    with open(filePath) as f:
+        try:
+            json.load(f)
+        except Exception as e:
+            print('')
+            print('----------------------------------------')
+            print('ERROR:')
+            print("{} error: {}".format(filePath, e))
+            print('----------------------------------------')
+            print('')
+            exit(1)
+
+#
+# Apply lint
+#
+
+subprocess.run([
+    "{}/apply-lint.sh".format(os.path.dirname(__file__)),
+    "{}/*.json".format(publicJsonDirectory)
+])
