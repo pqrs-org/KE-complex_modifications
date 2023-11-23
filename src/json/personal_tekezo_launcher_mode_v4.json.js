6d80a5688a8fa65f2abd161cf7b3c7f559461291
Takayama Fumihiko <tekezo@pqrs.org>
2023-11-23 19:53:37 +0900

    Update personal_tekezo_launcher_mode_v4 (do not call shell if the application is already focused)

--- src/json/personal_tekezo_launcher_mode_v4.json.js
+++ src/json/personal_tekezo_launcher_mode_v4.json.js
@@ -45,37 +45,72 @@ function generateLauncherMode(
   /**
    * @type {{
    *   bundleIdentifier?: string,
-   *   to: any[],
+   *   to?: any[],
    * }} */
-  definition
+  options
 ) {
+  const result = []
+
   var to = []
-  if (definition.bundleIdentifier !== undefined) {
+  if (options.bundleIdentifier !== undefined) {
     to.push({
-      shell_command: "open -b '" + definition.bundleIdentifier + "'",
+      shell_command: "open -b '" + options.bundleIdentifier + "'",
     })
   }
-  if (definition.to !== undefined) {
-    to = to.concat(definition.to)
+  if (options.to !== undefined) {
+    to = to.concat(options.to)
   }
 
-  return [
-    {
+  const definitions = []
+
+  if (options.bundleIdentifier !== undefined) {
+    //
+    // Do not call shell if the application is already focused.
+    //
+    // In the combination of macOS 14.1.1 and Google Chrome 119,
+    // there is a problem that keyboard shortcuts of switching windows are ignored until
+    // Google Chrome is unfocused if shell_command is called when the application is already frontmost.
+    //
+    // To avoid this problem, we skip shell_command when the application is focused.
+    //
+    definitions.push({
+      extraConditions: [
+        {
+          type: 'frontmost_application_if',
+          bundle_identifiers: ['^' + options.bundleIdentifier.replace(/\./g, '\\.') + '$'],
+        },
+      ],
+      extraTo: [],
+    })
+  }
+
+  // Call shell command if the application is not frontmost.
+  definitions.push({
+    extraConditions: [],
+    extraTo: to,
+  })
+
+  definitions.forEach(function (def) {
+    result.push({
       type: 'basic',
       from: {
         key_code: from_key_code,
         modifiers: { optional: ['any'] },
       },
-      to: to,
-      conditions: [
-        {
-          type: 'variable_if',
-          name: 'launcher_mode_v4',
-          value: 1,
-        },
-      ],
-    },
-    {
+      to: def.extraTo,
+      conditions: [].concat(
+        [
+          {
+            type: 'variable_if',
+            name: 'launcher_mode_v4',
+            value: 1,
+          },
+        ],
+        def.extraConditions
+      ),
+    })
+
+    result.push({
       type: 'basic',
       from: {
         simultaneous: [{ key_code: parameters.trigger_key }, { key_code: from_key_code }],
@@ -100,12 +135,15 @@ function generateLauncherMode(
             value: 1,
           },
         },
-      ].concat(to),
+      ].concat(def.extraTo),
+      conditions: def.extraConditions,
       parameters: {
         'basic.simultaneous_threshold_milliseconds': parameters.simultaneous_threshold_milliseconds,
       },
-    },
-  ]
+    })
+  })
+
+  return result
 }
 
 main()
