9f899d887810d3ea184188aec1befbcc454875b4
Peter Kingswell <peterkingswell@gmail.com>
2024-01-04 05:23:20 -0500

    add support for Command_R+.

--- src/json/change_command_l-and_period_to_escape.js
+++ src/json/change_command_l-and_period_to_escape.js
@@ -2,7 +2,7 @@ function main() {
   console.log(
     JSON.stringify(
       {
-        title: 'Change Command_L + Period to Escape',
+        title: 'Change Command_L|Command_R + Period to Escape',
         rules: [
           {
             description: 'Change Command_L + Period to Escape, to aid those who use the iPad Magic Keyboard since Command_L + Period is Escape on that keyboard.',
@@ -13,6 +13,21 @@ function main() {
                   key_code: 'period',
                   modifiers: {
                     mandatory: ['left_command'],
+                  },
+                },
+                to: [{ key_code: 'escape' }],
+              },
+            ],
+          },
+          {
+            description: 'Change Command_R + Period to Escape, to aid those who use the iPad Magic Keyboard since Command_R + Period is Escape on that keyboard.',
+            manipulators: [
+              {
+                type: 'basic',
+                from: {
+                  key_code: 'period',
+                  modifiers: {
+                    mandatory: ['right_command'],
                   },
                 },
                 to: [{ key_code: 'escape' }],
